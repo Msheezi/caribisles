bbf0a4762f976172afe217ba570a5dbb43e7db78
msheezi <marshall.randolph@gmail.com>
2019-10-15 13:06:04 -0700

    Add temp store

--- src/leftbar.jsx
+++ src/leftbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Collapsible from './collapsible'
+import {islands, resorts} from './store'
 
 const SidebarContainer = styled.div`
 margin-top: 10px;
@@ -36,20 +37,6 @@ const Panel = styled.div`
   transition: max-height 0.2s ease-out;
 
 `
-
-const islands = {
-  0: {name: "St.Kits", id: 0},
-  1: { name: "Barbados", id: 1},
-  2: { name: "St.Lucia", id: 2}
-}
-
-const resorts = {
-  0: { name: "First Resort", islandId: 0} ,
-  1: { name: "Second Resort", islandId: 0 } ,
-  2: { name: "Third Resort", islandId: 1 } ,
-  3: { name: "Fourth Resort", islandId: 1 } ,
-  4: { name: "Fifth Resort", islandId: 2 } 
-}
 
 
 
---
+++ src/store.js
@@ -0,0 +1,14 @@
+
+export const islands = {
+    0: { name: "St.Kits", id: 0 },
+    1: { name: "Barbados", id: 1 },
+    2: { name: "St.Lucia", id: 2 }
+}
+
+export const resorts = {
+    0: { name: "First Resort", islandId: 0 },
+    1: { name: "Second Resort", islandId: 0 },
+    2: { name: "Third Resort", islandId: 1 },
+    3: { name: "Fourth Resort", islandId: 1 },
+    4: { name: "Fifth Resort", islandId: 2 }
+}
